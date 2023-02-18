8d92b0d9734c78b25da8c2b2e320b18e409fb4ba
fikretcure <fikretcure@gmail.com>
2023-02-18 19:08:07 +0300

    Updated createRegistrationCode in RegistrationCodeTrait

--- app/Traits/RegistrationCodeTrait.php
+++ app/Traits/RegistrationCodeTrait.php
@@ -2,17 +2,18 @@
 
 namespace App\Traits;
 
-use App\Models\User;
 
 /**
  *
  */
 trait RegistrationCodeTrait
 {
+
     /**
+     * @param $model
      * @return array
      */
-    public function createRegistrationCode($model)
+    public function createRegistrationCode($model): array
     {
         $last_id = $model::query()->withTrashed()->max("id") + 1;
         return ["registration_code" => $model::$registration_code . str_repeat("0", (4 - strlen($last_id))) . $last_id];
