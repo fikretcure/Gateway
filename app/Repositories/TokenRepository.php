e3a82a5204603064bc5daceca98f2b91298a4111
fikretcure <fikretcure@gmail.com>
2023-02-18 19:06:10 +0300

    Updated TokenRepository

--- app/Repositories/TokenRepository.php
+++ app/Repositories/TokenRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories;
 
-use App\Models\User;
+use App\Models\Token;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  *
  */
-class UserRepository extends Repository
+class TokenRepository extends Repository
 {
 
     /**
@@ -24,7 +24,7 @@ class UserRepository extends Repository
      */
     public function __construct()
     {
-        $this->model = User::class;
+        $this->model = Token::class;
     }
 
     /**
