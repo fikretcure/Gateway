655e7627cd4030d249a09f91646bcbd561d38838
fikretcure <fikretcure@gmail.com>
2023-02-19 17:39:11 +0300

    Added LoginShippedJob

---
+++ app/Jobs/LoginShippedJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Traits\SenRequestTrait;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class LoginShippedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use SenRequestTrait;
+
+    public array $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->sendRequest("emailservice.test/api/shipped", "post", [
+            "type" => "login",
+            "emails" => [
+                $this->data["email"]
+            ],
+            "user" => $this->data["name"],
+            "date" => now()->format("d/m/y H:i"),
+            "http_user_agent" => $this->data["http_user_agent"]
+        ]);
+    }
+}
